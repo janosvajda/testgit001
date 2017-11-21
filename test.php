a9424e18d41d0968cac25be330089f86c0a16c3c
Janos Vajda <vajdajanos@gmail.com>
2017-11-21 10:25:04 +0000

    commit feature/lalala2 commit 1

--- test.php
+++ test.php
@@ -4,3 +4,7 @@ echo "hello";
 
 
 echo "this is other feature";
+
+
+$a = 'lalala2';
+echo $a1;
---
+++ test2_lalala2.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: janos.vajda
+ * Date: 21/11/17
+ * Time: 10:24
+ */
+
+echo "lalalal1";
